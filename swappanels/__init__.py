b2359f9680f9fa144a0005cde9d1d9136e86949e
Richard Guay <raguay@customct.com>
2018-02-16 08:52:38 +0700

    Working on selections moving over on swap

--- swappanels/__init__.py
+++ swappanels/__init__.py
@@ -1,11 +1,28 @@
-from fman import DirectoryPaneCommand, show_alert
+from fman import DirectoryPaneCommand
 import os
-import zipfile
+
 
 class SwapPanel(DirectoryPaneCommand):
     def __call__(self):
         panes = self.pane.window.get_panes()
-        rpane = panes[0].get_path()
-        lpane = panes[1].get_path()
-        panes[0].set_path(lpane)
-        panes[1].set_path(rpane)
+
+        lpane = panes[0]
+        lpane_path = lpane.get_path()
+        lpane_selection = lpane.get_selected_files()
+
+        rpane = panes[1]
+        rpane_path = rpane.get_path()
+        rpane_selection = rpane.get_selected_files()
+
+        def _setLeftPaneSelections():
+            if(lpane_selection):
+                for url in lpane_selection:
+                    rpane.toggle_selection(url)
+
+        def _setRightPaneSelections():
+            if(rpane_selection):
+                for url in rpane_selection:
+                    lpane.toggle_selection(url)
+
+        lpane.set_path(rpane_path, _setLeftPaneSelections)
+        rpane.set_path(lpane_path, _setRightPaneSelections)
